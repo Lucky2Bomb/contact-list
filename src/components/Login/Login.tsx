c875b875df389aca7714a59926a92c256afc3d50
Airat Ahunov <lucky2bomb@gmail.com>
2022-03-19 19:19:01 +0500

    added margin and navigate to register

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,42 +1,45 @@
-import { Button, Grid, Paper, TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import { REGISTER } from '../../routes'
+import AuthLayout from '../Layout/AuthLayout'
 import './login.scss'
 
 function Login() {
+  const navigate = useNavigate()
   return (
-    <Grid
-      container
-      direction="column-reverse"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Paper elevation={10} className="login" sx={{ p: 5 }}>
-        <h1>Sign in</h1>
+    <AuthLayout>
+      <h1>Sign in</h1>
 
-        <TextField
-          id="login"
-          label="login"
-          variant="standard"
-          required
-          fullWidth
-        />
-        <TextField
-          id="password"
-          label="password"
-          variant="standard"
-          type="password"
-          autoComplete="current-password"
-          required
-          fullWidth
-        />
+      <TextField
+        id="login"
+        label="Login"
+        variant="standard"
+        required
+        fullWidth
+      />
+      <TextField
+        id="password"
+        label="Password"
+        variant="standard"
+        type="password"
+        autoComplete="current-password"
+        sx={{ mt: 1 }}
+        required
+        fullWidth
+      />
 
-        <Button variant="contained" fullWidth sx={{ mt: 3 }}>
-          SIGN IN
-        </Button>
-        <Button variant="text" fullWidth sx={{ mt: 1 }}>
-          SIGN UP
-        </Button>
-      </Paper>
-    </Grid>
+      <Button variant="contained" fullWidth sx={{ mt: 3 }}>
+        SIGN IN
+      </Button>
+      <Button
+        variant="text"
+        fullWidth
+        sx={{ mt: 1 }}
+        onClick={() => navigate(`/${REGISTER.route}`)}
+      >
+        SIGN UP
+      </Button>
+    </AuthLayout>
   )
 }
 
